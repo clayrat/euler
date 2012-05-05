87804224a879fa58043cbd21aae8726f29338881
unknown <alhry@jambavan.psb.UGent.be>
2012-05-05 21:59:59 +0200

    1

--- src/p001.scala
+++ src/p001.scala
@@ -6,5 +6,5 @@ object p001 {
   def from(n: Int): Stream[Int] = n #:: from(n + 1)
   val nats = from(0)
   val answer = nats filter (x => (x % 3 == 0) || (x % 5 == 0) )
-  def main(args:Array[String]) { println(answer.takeWhile(x=>(x<1000)).sum) }
+  def main(args:Array[String]) { println(answer.takeWhile(x=>(x < 1000)).sum) }
 }
